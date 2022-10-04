5df2e6347cc7d3aa6d4a4e31e3b71da5781d9ee1
Donghyeon Lee <earthicko@gmail.com>
2022-10-04 16:21:04 +0900

    마지막 프로세스의 반환 상태를 그대로 반환하도록

--- src/executor/forker.c
+++ src/executor/forker.c
@@ -13,10 +13,31 @@
 #include "executor.h"
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/wait.h>
+
+int	execute_child(t_execinfo *e, int i_proc)
+{
+	if (set_stdin_stdout(e, i_proc))
+		return (-1);
+	return (execve(e->bins[i_proc], e->args[i_proc], e->envp));
+}
+
+int	wait_for_last_child(pid_t pid)
+{
+	int		stat_loc;
+
+	if (waitpid(pid, &stat_loc, 0) < 0)
+	{
+		perror(NULL);
+		return (-1);
+	}
+	return (WEXITSTATUS(stat_loc));
+}
 
 int	fork_ntimes(t_execinfo *e, int n)
 {
 	pid_t	pid;
+	int		i_proc;
 
 	if (n <= 0)
 		return (0);
@@ -27,12 +48,11 @@ int	fork_ntimes(t_execinfo *e, int n)
 		return (-1);
 	}
 	if (pid == 0)
-	{
-		n--;
-		if (set_stdin_stdout(e, n))
-			return (-1);
-		return (execve(e->bins[n], e->args[n], e->envp));
-	}
+		return (execute_child(e, e->n_proc - n));
 	else
+	{
+		if (n == 1)
+			return (wait_for_last_child(pid));
 		return (fork_ntimes(e, n - 1));
+	}
 }
